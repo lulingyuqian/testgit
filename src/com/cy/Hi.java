e485469a53b951d05756c7732d5ed337e1ff5241
lulingyuqian <66237872+lulingyuqian@users.noreply.github.com>
2020-06-01 17:41:17 +0800

    Update Hi.java

--- src/com/cy/Hi.java
+++ src/com/cy/Hi.java
@@ -5,6 +5,8 @@ import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 public class Hi {
     public static void main(String[] args) {
         System.out.println("hahaha");
+        System.out.println("hahaha");
+        System.out.println("hahaha");
     }
 
 }
